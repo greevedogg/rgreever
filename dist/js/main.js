82454d85d201b14161af04d76dcb45056d8d4db8
greevedogg <greevedogg@gmail.com>
2016-08-26 14:53:54 -0400

    MAJOR REFACTORING

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,5 +4,27 @@ $(document).ready(function(){
     });
 });
             
+function checkScroll(){
+    var startY = $('.header').height() * 1; //The point where the navbar changes in px
 
+    if($(window).scrollTop() > startY){
+        $('.header').addClass("scrolled");
+        $('.link').addClass("scrolled");
+    }else{
+        $('.header').removeClass("scrolled");
+        $('.link').removeClass("scrolled");
+    }
+}
+
+if($('.header').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+if($('.link').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
  
